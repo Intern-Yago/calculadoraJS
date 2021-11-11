1916a3d590f2509770eb00e2d2320ac2ecc38c93
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-11-11 20:43:35 -0300

    configura porta do heroku

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ server.use(express.urlencoded({extends: true}))
 //rotas
 server.use(routes)
 
-server.listen(8000, ()=>{
+server.listen(process.env.PORT || 8000, ()=>{
     console.log("Servidor rodando!\nVisit: http://localhost:8000\n")
 })
