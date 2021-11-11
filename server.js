bde92edbf8823af9a99d2c9904eb8ebeee31c3d6
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-11-11 20:16:52 -0300

    node web server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const server = express()
+const routes = require("./routes")
+
+//habilitar arquivos statics
+server.use(express.static("public"))
+
+server.use(express.urlencoded({extends: true}))
+
+//rotas
+server.use(routes)
+
+server.listen(8000, ()=>{
+    console.log("Servidor rodando!\nVisit: http://localhost:8000\n")
+})
